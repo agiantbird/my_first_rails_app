4aab124a27841835222165195fac9e0b2d0db020
Rich Hoyt <rhoyt10@gmail.com>
2020-06-16 23:18:32 -0400

    Convert to blog project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_204614) do
+ActiveRecord::Schema.define(version: 2020_06_17_005713) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
