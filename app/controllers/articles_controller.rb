d2781efea9ed58ce0cb94d43635aa456113b8914
Rich Hoyt <rhoyt10@gmail.com>
2020-06-20 20:42:51 -0400

    Add comments functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,10 +29,14 @@ class ArticlesController < ApplicationController
 
 	def new
 		@article = Article.new
+
 	end
 
 	def show
 		@article = Article.find(params[:id])
+
+		@comment = Comment.new
+		@comment.article_id = @article.id
 	end
 
 	def update
