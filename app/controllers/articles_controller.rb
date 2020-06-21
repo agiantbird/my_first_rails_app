e67b52ddb1831c147126fa97ebef7a411a4e6f7d
Rich Hoyt <rhoyt10@gmail.com>
2020-06-20 23:26:14 -0400

    Add tags, remove unused code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,51 +1,50 @@
 class ArticlesController < ApplicationController
-	include ArticlesHelper
+  include ArticlesHelper
+  
+  def index
+    @articles = Article.all
+  end
 
-	def create
-		@article = Article.new(article_params)
-		@article.save
+  def show
+    @article = Article.find(params[:id])
+    
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
 
-		flash.notice = "Article '#{@article.title}' Created!"
+  def new
+    @article = Article.new
+  end
 
-		redirect_to article_path(@article)
-	end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-	def destroy
-		@article = Article.find(params[:id])
-		@article.destroy
+  def create
+    @article = Article.new(article_params)
+    @article.save
 
-		flash.notice = "Article '#{@article.title}' Deleted!"
+    flash.notice = "Article '#{@article.title}' Created!"
 
-		redirect_to articles_path
-	end
+    redirect_to article_path(@article)
+  end
 
-	def edit
-		@article = Article.find(params[:id])
-	end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
 
-	def index
-		@articles = Article.all
-	end
+    flash.notice = "Article '#{@article.title}' Updated!"
 
-	def new
-		@article = Article.new
+    redirect_to article_path(@article)
+  end
 
-	end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-	def show
-		@article = Article.find(params[:id])
+    flash.notice = "Article '#{@article.title}' Deleted!"
 
-		@comment = Comment.new
-		@comment.article_id = @article.id
-	end
-
-	def update
-		@article = Article.find(params[:id])
-		@article.update(article_params)
-
-		flash.notice = "Article '#{@article.title}' Updated!"
-
-		redirect_to article_path(@article)
-	end
+    redirect_to articles_path
+  end
 
 end
